b9f034923ae3fe4681efc2007991bff3105386c0
Luis Rios <luismrios1602@gmail.com>
2025-11-01 00:52:49 -0500

    Initial commit

---
+++ src/main/java/com/luzinho/sbuar/SbuarApplication.java
@@ -0,0 +1,13 @@
+package com.luzinho.sbuar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbuarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbuarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luzinho/sbuar/SbuarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luzinho.sbuar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbuarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
