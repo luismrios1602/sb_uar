8f544cf494ffaaee8ca5c8fd0358c14c7e202c39
Luis Rios <luismrios1602@gmail.com>
2025-11-04 00:32:41 -0500

    Chapter 5: Add Configuration Properties by Value and ConfigProp Component/Class

---
+++ src/main/java/com/luzinho/sbuar/UserController.java
@@ -0,0 +1,54 @@
+package com.luzinho.sbuar;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("chapter5/user")
+public class UserController {
+
+    @Value("${user_name: Luis}")
+    String nameValue;
+    @Value("${user_lastName: Rios}")
+    String lastNameValue;
+    @Value("${user_fullName: ${user_name} ${user_lastName} } ")
+    String fullName;
+
+    final UserConfigProps userConfigProps;
+
+    UserController(UserConfigProps userProps){
+        this.userConfigProps = userProps;
+    }
+
+    @GetMapping("nameValue")
+    String getNameValue(){
+        return nameValue;
+    }
+
+    @GetMapping("lastNameValue")
+    String getLastNameValue(){
+        return lastNameValue;
+    }
+
+    @GetMapping("fullNameValue")
+    String getFullName(){
+        return fullName;
+    }
+
+    @GetMapping("nameProp")
+    String getNameProp(){
+        return userConfigProps.getName();
+    }
+
+    @GetMapping("lastNameProp")
+    String getLastNameProp(){
+        return userConfigProps.getLastName();
+    }
+
+    @GetMapping("fullNameProp")
+    String getFullNameProp(){
+        return userConfigProps.getFullName();
+    }
+}
